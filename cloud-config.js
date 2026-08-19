2509129b12efd498b7f41a366842b029e66d09ee
jerry6088 <jerry6088@gmail.com>
2026-08-19 13:11:17 -0500

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -3,6 +3,6 @@
 // The publishable/anon key is intended for browser use when Row Level Security is enabled.
 // NEVER put a service_role key here.
 window.HM_CLOUD_CONFIG = {
-  url: https://gnyuigmuzvodobbvdhrp.supabase.co
-  sb_publishable_2Bzt-JAF74bz9hleWUkH1A_Gd1kEe9f
+  url: "https://gnyuigmuzvodobbvdhrp.supabase.co",
+  key:  "sb_publishable_2Bzt-JAF74bz9hleWUkH1A_Gd1kEe9f"
 };
